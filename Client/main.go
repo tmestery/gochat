2f2a8d4582e0eb3fe570927648e2895bc9b42290
Tyler Mestery <tyler@mestery.com>
2025-12-23 22:02:50 -0600

    Implementing partial CLI with options for client-server communication

--- Client/main.go
+++ Client/main.go
@@ -11,6 +11,10 @@ type Item struct {
 	Body string
 }
 
+func menuOptions() {
+	fmt.Println("\n-------------gochat command options-------------\n1. Get Database\n2. Get Item By Name\n3. Add Item\n4. Edit Item\n5. Delete Item\n6. Exit")
+}
+
 func main() {
 	var reply Item
 	var db []Item
@@ -22,17 +26,40 @@ func main() {
 		log.Fatal("Connection Error: ", err)
 	}
 
+	var running bool = true
+	var response int
+
+	for running {
+		menuOptions()
+		fmt.Scan(&response)
+
+		switch response {
+		case 1:
+			fmt.Println("Database: ", db)
+		case 2:
+
+		case 3:
+
+		case 4:
+
+		case 5:
+		
+		default:
+			fmt.Println("\nExiting gochat, thank you!")
+			running = false
+			break
+		}
+	}
+
+
+
+
 	a := Item{"1st", ""}
 	b := Item{"2nd", "A second item"}
-
-	fmt.Println("Commands:\n1. Add Item\n\n")
-	fmt.Scan(&choice)
 
 	if choice == 1 {
 		client.Call("API.AddItem", a, &reply)
 		client.Call("API.AddItem", b, &reply)
 		client.Call("API.GetDB", "", &db)
-
-		fmt.Println("Database: ", db)
 	}
 }
